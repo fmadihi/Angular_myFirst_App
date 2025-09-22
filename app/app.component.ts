568e8017cba98f0aea1bf91be3cc1cddd73f33e4
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2025-09-22 17:01:47 +0330

    Add files via upload

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,23 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { Practice001Component } from './practice001/practice001.component';
 import { Practice002Component } from './practice002/practice002.component';
-import { TwoComponentComponent } from "./two-component/two-component.component";
+import { TwoComponentComponent } from './two-component/two-component.component';
 import { DirektiveComponent } from './direktive/direktive.component';
 import { Basic_Background_highlighte_Directive } from './direktive/customDirective/Basic_Background_highlighte.directive';
 import { RenderBasicBackgroundHilighteDirective } from './direktive/customDirective/render-basic-background-hilighte.directive';
-
+import { StructureDirectiveComponent } from './structure-directive/structure-directive.component';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ServerComponent, ServersComponent, Practice001Component, Practice002Component, TwoComponentComponent,TwoComponentComponent,DirektiveComponent,Basic_Background_highlighte_Directive,RenderBasicBackgroundHilighteDirective],
+  imports: [
+    RouterOutlet,
+    ServerComponent,
+    ServersComponent,
+    Practice001Component,
+    Practice002Component,
+    TwoComponentComponent,
+    TwoComponentComponent,
+    DirektiveComponent,
+    Basic_Background_highlighte_Directive,
+    RenderBasicBackgroundHilighteDirective,
+    StructureDirectiveComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'first_app';
   name = 'Fatemeh Madihi';
 
+  constructor(private userService: UserService) {}
+  ngOnInit(): void {
+    this.userService.getData();
+  }
 }
---
+++ app/user.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+  getData(){
+    console.log("user service from getdata");
+    
+  }
+}
